23f9b47b954b7a43c14c8bc350567d93edcb0c6e
Gudmundsson <Mats.Gudmundsson@clasohlson.se>
2020-10-18 00:17:05 +0200

    small changes, css, and tested some Hooks.

--- my-app-react/src/view/loginview/LoginView.js
+++ my-app-react/src/view/loginview/LoginView.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min'
 
 const LoginView = () => {
+
+    const history = useHistory();
+    const [loginEmail, setLoginEmail] = useState();
+    const [loginPassword, setLoginPassword] = useState();
+    console.log(loginEmail);
+    console.log(loginPassword);
+
+    const login = () => {
+
+        alert(loginEmail + "Has logged in");
+        history.push("/");
+        localStorage.setItem('userName', loginEmail);
+    }
+
     return (
         <div>
             <p>Log in</p>
-            <input></input>
-<input></input>
+            Email:<input onChange={e => setLoginEmail(e.target.value)}></input><br />
+            Passw:<input onChange={e => setLoginPassword(e.target.value)}></input><br />
+            <button onClick={() => login()}>Log in</button>
         </div>
     )
+
+
 }
 
 export default LoginView
--- my-app-react/src/view/startpage/Startpage.js
+++ my-app-react/src/view/startpage/Startpage.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import { cleanup } from '@testing-library/react';
+import React, { useEffect } from 'react';
 
 function Startpage() {
+
+    const [random, setRandom] = useState();
+
+    useEffect(() => {
+        const userName = localStorage.getItem('userName');
+
+        if (userName) {
+            alert("Hello, MR" + userName);
+        }
+
+
+    }, [])
+
     return (
         <div>
             startpage
