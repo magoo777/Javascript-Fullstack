c2cd91faef9384cd456997394c235e55ccfa5b0d
Mats <mats@jawlock.com>
2020-11-27 12:31:41 +0100

    fixed some design

--- my-app-react/src/components/PlayerPerfComp/performance.js
+++ my-app-react/src/components/PlayerPerfComp/performance.js
@@ -1,19 +1,21 @@
 import React from 'react';
 
+import playerstats from '../../shared/images/playerstats.png'
+
 
 // importerar css för komponenten
 import './performance.css';
 
 const Performance = (props) => {
 
-    const {gender, hair_color, skin_color } = props
+    const { gender, hair_color, skin_color } = props
 
     return (
         <div className="playerPerformance" >
             <h3>{gender}</h3>
             <h3>{hair_color}</h3>
             <h3>{skin_color}</h3>
-            <p>Kanske visa data med hjälp av d3 react....</p>
+            <img src={playerstats} alt="Kanske visa data med hjälp av d3 react..." />
         </div>
     )
 }
