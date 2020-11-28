e0b20615c90b6ad2f5c43484073d05c73eff43c9
Mats <mats@jawlock.com>
2020-11-28 14:50:02 +0100

    herokuuu

--- configuration/Configuration.js
+++ configuration/Configuration.js
@@ -7,7 +7,7 @@ dotenv.config()
 
 const connectToDatabase = async () => {
 	try {
-		const DB_URL = process.env.DATABASE_URL
+		// const DB_URL = process.env.DATABASE_URL
 		await mongoose.connect(DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
 		console.log('Connected to DB')
 	} catch (error) {
