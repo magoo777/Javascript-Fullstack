358236417b32c9757565c0ddac0fb252d3814c29
Mats <mats@jawlock.com>
2020-11-28 17:23:05 +0100

    herokuuu

--- Server.js
+++ Server.js
@@ -25,7 +25,7 @@ app.use(morgan('common'))
 app.use(cors())
 
 if (process.env.ENVIROMENT === 'production') {
-    app.use(express.static('test-app-react/build'))
+    app.use(express.static('static'))
 }
 //app.use(express.static('test-app-react/build'))
 // app.use(express.static('static'))
