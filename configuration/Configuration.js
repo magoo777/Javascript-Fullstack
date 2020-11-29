c4fba454553b3184599b934ae8df3ee30bed4510
Mats <mats@jawlock.com>
2020-11-29 09:26:36 +0100

    herokuuu

--- configuration/Configuration.js
+++ configuration/Configuration.js
@@ -20,7 +20,7 @@ const connectToDatabase = async () => {
 }
 
 const connectToPort = (app) => {
-	const port = process.env.PORT || 3001
+	const port = process.env.PORT || 5000
 	app.listen(port, () => {
 		console.log(`Running on porten: ${port}`)
 	})
